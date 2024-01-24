0216370fe6bfb02665752e065583c73ee0d5b900
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-24 16:06:47 -0700

    finished strangeSums

--- strangeSums.js
+++ strangeSums.js
@@ -1,5 +1,16 @@
-// your code here
-
+function strangeSums(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let num1 = arr[i];
+    for (let j = i + 1; j < arr.length; j++) {
+      let num2 = arr[j];
+      if (num1 + num2 == 0) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
 
 console.log(strangeSums([2, -3, 3, 4, -2]));     // 2
 console.log(strangeSums([42, 3, -1, -42]));      // 1
