68f1ca47de47225c7dd2842b6d7224465ae2aea2
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-24 15:54:32 -0700

    initial commit

---
+++ pairProduct.js
@@ -0,0 +1,12 @@
+// your code here
+
+
+
+
+console.log(pairProduct([4, 2, 5, 8], 16))    // true
+console.log(pairProduct([8, 1, 9, 3], 8))     // true
+console.log(pairProduct([3, 4], 12))          // true
+console.log(pairProduct([3, 4, 6, 2, 5], 12)) // true
+console.log(pairProduct([4, 2, 5, 7], 16))    // false
+console.log(pairProduct([8, 4, 9, 3], 8))     // false
+console.log(pairProduct([3], 12))             // false
