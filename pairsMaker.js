b9641d91beb5dc5bc6a0c51410e9e0d6c478f3b8
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-24 15:26:20 -0700

    finished pairsMaker

--- pairsMaker.js
+++ pairsMaker.js
@@ -1,8 +1,14 @@
-// your code here
+function pairsMaker(arr) {
+  let pairs = [];
 
-
-
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      let pair = [arr[i], arr[j]];
+      pairs.push(pair);
+    }
+  }
+  return pairs;
+};
 
 
 console.log(pairsMaker(['a', 'b', 'c', 'd'])); // =>
