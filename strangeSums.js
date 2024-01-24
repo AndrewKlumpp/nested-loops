4280f628c7658f367dee665308ace8c9415f93fa
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-24 16:01:33 -0700

    initial commit

---
+++ strangeSums.js
@@ -0,0 +1,8 @@
+// your code here
+
+
+console.log(strangeSums([2, -3, 3, 4, -2]));     // 2
+console.log(strangeSums([42, 3, -1, -42]));      // 1
+console.log(strangeSums([-5, 5]));               // 1
+console.log(strangeSums([19, 6, -3, -20]));      // 0
+console.log(strangeSums([9]));                   // 0
