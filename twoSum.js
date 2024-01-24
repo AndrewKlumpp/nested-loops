b751d9feb89d86ea7463352f12e94c311afe342b
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-24 15:44:58 -0700

    finished twoSum

--- twoSum.js
+++ twoSum.js
@@ -1,7 +1,17 @@
-// your code here
+function twoSum(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    let num1 = arr[i];
 
+    for (let j = i + 1; j < arr.length; j++) {
+      let num2 = arr[j];
 
-
+      if (num1 + num2 == target) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
 
 
 
