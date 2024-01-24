caf8b3cf9a0b8428c7dfc060371c814b6529dfba
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-24 13:08:03 -0700

    initial commit

---
+++ pairsMaker.js
@@ -0,0 +1,19 @@
+// your code here
+
+
+
+
+
+
+console.log(pairsMaker(['a', 'b', 'c', 'd'])); // =>
+// [ [ 'a', 'b' ],
+//   [ 'a', 'c' ],
+//   [ 'a', 'd' ],
+//   [ 'b', 'c' ],
+//   [ 'b', 'd' ],
+//   [ 'c', 'd' ] ]
+
+console.log(pairsMaker(['Rosemary', 'Alex', 'Connor'])); // =>
+// [ [ 'Rosemary', 'Alex' ],
+//   [ 'Rosemary', 'Connor' ],
+//   [ 'Alex', 'Connor' ] ]
